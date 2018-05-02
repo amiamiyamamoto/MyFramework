34052b20e545ea5d1846fb1b9a455f6a0e7cb2f3
AMI <a_yamamoto@innovation.co.jp>
2018-05-02 10:02:26 +0900

    HasuminControllerをdatabaseを扱えるbassControllerから継承させる

--- code/controller/HasuminController.php
+++ code/controller/HasuminController.php
@@ -1,9 +1,11 @@
 <?php
-class HasuminController extends BaseController {
-    public function action() {
+class HasuminController extends DbConnectController{
+    public function dbAction() {
         //テンプレート取得
         $template = $this->getTemplate('hasumin.html');
-        
+        //dbの処理
+        $this->query("insert into users (name,age) values ('hasukon', 50)");
+        $this->query("aaaaaaainsert into users (name,age) values ('hasukon', 50)");
         //画面に表示
         $this->displayHtml($template);
     }
